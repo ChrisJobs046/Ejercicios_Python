654b3b8eaa52bea8acb9f980ad3084b61ff0c6c0
ChrisJobs046 <ChrisJobs046@gmail.com>
2020-07-16 00:36:39 -0400

    Probando

---
+++ Primer_Algoritmo/Prueba.py
@@ -0,0 +1,59 @@
+# Recuerden siempre tabular bien en python para que este lea el codigo correctamente.👍
+#print("soy el mejor")
+
+'''
+Nombre = "Salvador"
+
+Apellido = "Ramon"
+
+Edad = 15
+'''
+
+
+# [Entrada]
+
+# f"{}" esto sirve para concatenar.
+# el signo de +, tambien sirve para concatenar.
+# str() Sirve para convertir un valor numerico en un string.
+
+'''
+print(f"{Nombre} - {Apellido} {str(Edad)}")
+
+Pregunta = input("¿Cual es tu nombre?:")
+
+print("El nombre del usuario es: "+ Pregunta)
+'''
+
+# [Condiciones]
+''' el tema de las condiciones las hice en el programa de signo del zodiaco😂'''
+
+# Funciones
+'''Recuerden que las funciones sirven practicamente para reutilizar codigo'''
+'''return sirve para sacar informacion de una funcion, ya qie por defauk cualquier valor tratado dentro de la funcion se cada dentro
+   de esta.
+'''
+
+'''var_altura = int(input("¿Cual es tu altura?: "))
+
+def MostrarAltura(altura):
+    
+    resultado = ""
+
+    if altura >= 180:
+        resultado = ("Eres una persona alta")
+    else:
+        resultado = ("Eres bajito!!! por lo tanto tu novia no te ama😭😭😭 ")
+
+    return resultado
+
+print (MostrarAltura(var_altura))
+'''
+
+# Listas
+
+Nombres = ["Ramon", "juan", "Lucas"]
+
+print("el nombre es: " + Nombres[0])
+
+
+
---
+++ Signo_Zodiaco/Main.py
@@ -0,0 +1,15 @@
+# [Condiciones]
+
+# [Recordatorio]
+''' recuerden que a la hora de introducir datos en el input, poner el int() y encerrarlo todo en el,
+    si no dara error ya que el input por defaul solo recibe string 
+'''
+Pregunta = int(input("¿En que dia naciste?: "))
+
+
+
+if Pregunta >= 30:
+    print("Eres virgo")
+else:
+    print("eres aries")
+    
